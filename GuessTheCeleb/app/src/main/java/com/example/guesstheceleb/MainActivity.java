8c97432aeb8be4cad64dcb1c057d180aebd8b274
Weronika Żak <weronikarzak@gmail.com>
2019-12-03 22:28:39 +0100

    hiker's watch

--- GuessTheCeleb/app/src/main/java/com/example/guesstheceleb/MainActivity.java
+++ GuessTheCeleb/app/src/main/java/com/example/guesstheceleb/MainActivity.java
@@ -2,13 +2,173 @@ package com.example.guesstheceleb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    String[] anwers = new String[4];
+    int chosenCeleb = 0;
+    int locationOfCorrectAnswer = 0;
+    ImageView imageView;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    public class ImageDownloader extends  AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return  myBitmap;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection)url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char)data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.celebImage);
+
+        button0 = findViewById(R.id.button1);
+        button1 = findViewById(R.id.button2);
+        button2 = findViewById(R.id.button3);
+        button3 = findViewById(R.id.button4);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+            result = task.execute("http://www.posh24.se/kandisar").get();
+
+            String[] splitResult = result.split("<div class=\"listedArticles\">");
+
+            Pattern p = Pattern.compile("img src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+                celebURLs.add(m.group(1));
+            }
+
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+                celebNames.add(m.group(1));
+            }
+
+            newQuestion();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void chooseCeleb(View view) {
+        if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+            Toast.makeText(getApplicationContext(), "Dobrze!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Źle! Prawidłowa odpowiedź to: " + celebNames.get(chosenCeleb), Toast.LENGTH_SHORT).show();
+        }
+        newQuestion();
+
+    }
+
+    public void newQuestion() {
+        try {
+            Random rand = new Random();
+            chosenCeleb = rand.nextInt(celebURLs.size());
+
+            ImageDownloader imageTask = new ImageDownloader();
+            Bitmap celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+
+            imageView.setImageBitmap(celebImage);
+
+            locationOfCorrectAnswer = rand.nextInt(4);
+
+            int incorrectAnswerLocation;
+
+            for (int i = 0; i < 4; i++) {
+                if (i == locationOfCorrectAnswer) {
+                    anwers[i] = celebNames.get(chosenCeleb);
+                } else {
+                    incorrectAnswerLocation = rand.nextInt(celebURLs.size());
+
+                    while (incorrectAnswerLocation == locationOfCorrectAnswer) {
+                        incorrectAnswerLocation = rand.nextInt(celebURLs.size());
+                    }
+
+                    anwers[i] = celebNames.get(incorrectAnswerLocation);
+                }
+            }
+
+            button0.setText(anwers[0]);
+            button1.setText(anwers[1]);
+            button2.setText(anwers[2]);
+            button3.setText(anwers[3]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- WhatsTheWeather/app/src/main/java/com/example/whatstheweather/MainActivity.java
+++ WhatsTheWeather/app/src/main/java/com/example/whatstheweather/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.whatstheweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -14,8 +20,22 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText cityText;
+    TextView weatherText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityText = findViewById(R.id.cityText);
+        weatherText = findViewById(R.id.weatherText);
+
+    }
 
     public  class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -40,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 return result;
             } catch (Exception e) {
                 e.printStackTrace();
+
                 return null;
             }
         }
@@ -53,34 +74,48 @@ public class MainActivity extends AppCompatActivity {
 
                 String weatherInfo = jsonObject.getString("weather");
 
-                Log.i("Weather content", weatherInfo);
-
                 JSONArray arr = new JSONArray(weatherInfo);
+
+                String message = "";
 
                 for (int i = 0; i < arr.length(); i++) {
                     JSONObject jsonPart = arr.getJSONObject(i);
 
-                    Log.i("main", jsonPart.getString("main"));
-                    Log.i("main", jsonPart.getString("description"));
+                    String main = jsonPart.getString("main");
+                    String description = jsonPart.getString("description");
+
+                    if (!main.equals("") && !description.equals(""))
+                        message += main + ": " + description + "\r\n";
+                }
+
+                if (!message.equals("")) {
+                    weatherText.setText(message);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Couldn't find weather :(", Toast.LENGTH_SHORT).show();
                 }
 
             } catch (Exception e) {
                 e.printStackTrace();
+
+                Toast.makeText(getApplicationContext(), "Couldn't find weather :(", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-    }
-
     public void showWeather(View view) {
-        DownloadTask downloadTask = new DownloadTask();
-        downloadTask.execute("");
+        try {
+            DownloadTask downloadTask = new DownloadTask();
+
+            String encodedCityName = URLEncoder.encode(cityText.getText().toString(), "UTF-8");
+
+            downloadTask.execute("https://api.openweathermap.org/data/2.5/weather?q=" + encodedCityName + "&appid=2d6ebecffd8059c307d9ad7214fc7bc4");
+
+            InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            mgr.hideSoftInputFromWindow(weatherText.getWindowToken(), 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Couldn't find weather :(", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
