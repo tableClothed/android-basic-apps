1525489ae369944b05546e716562ff0eb31d8478
Weronika Żak <weronikarzak@gmail.com>
2019-12-08 17:19:36 +0100

    notes

--- MemorablePlaces/app/src/main/java/com/example/memorableplaces/MainActivity.java
+++ MemorablePlaces/app/src/main/java/com/example/memorableplaces/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -25,10 +27,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.listView);
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.zappycode.memorableplaces", Context.MODE_PRIVATE);
 
-        places.add("Add a new place...");
-        locations.add(new LatLng(0, 0));
+        ArrayList<String> latitudes = new ArrayList<>();
+        ArrayList<String> lontidudes = new ArrayList<>();
+
+        places.clear();
+        lontidudes.clear();
+        latitudes.clear();
+
+        try {
+            places = (ArrayList<String>)ObjectSerializer.deserialize(sharedPreferences.getString("places", ObjectSerializer.serialize(new ArrayList<String>())));
+            latitudes = (ArrayList<String>)ObjectSerializer.deserialize(sharedPreferences.getString("lats", ObjectSerializer.serialize(new ArrayList<String>())));
+            lontidudes = (ArrayList<String>)ObjectSerializer.deserialize(sharedPreferences.getString("lons", ObjectSerializer.serialize(new ArrayList<String>())));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (places.size() > 0 && latitudes.size() > 0 && lontidudes.size() > 0) {
+            if (places.size() == latitudes.size() && places.size() == latitudes.size())
+                for (int i = 0; i < latitudes.size(); i++) {
+                    locations.add(new LatLng(Double.parseDouble(latitudes.get(i)), Double.parseDouble(lontidudes.get(i))));
+                }
+        } else {
+            places.add("Add a new place...");
+            locations.add(new LatLng(0, 0));
+        }
+
+        ListView listView = findViewById(R.id.listView);
 
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
 
---
+++ Notes/app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.notes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        notes.add("Example note..");
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), NotesEditorActivity.class);
+                intent.putExtra("noteId", position);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    //MENU
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.add_note) {
+            Intent intent = new Intent(getApplicationContext(), NotesEditorActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+
+        return  false;
+    }
+}
