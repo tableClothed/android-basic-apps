8bed8da73c770ae133aa0d3ca05fa0cc61b12616
Weronika Żak <weronikarzak@gmail.com>
2019-12-17 20:35:59 +0100

    Voice Input

--- AndroidWear/app/src/main/java/com/example/androidwear/MainActivity.java
+++ AndroidWear/app/src/main/java/com/example/androidwear/MainActivity.java
@@ -1,33 +1,44 @@
 package com.example.androidwear;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends WearableActivity {
-
-    private TextView mTextView;
-    int count = 0;
-
-    public void plusOne(View view) {
-        count ++;
-        mTextView.setText(Integer.toString(count));
-    }
-
-    public void reset(View view) {
-        count = 0;
-        mTextView.setText(Integer.toString(count));
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        startActivityForResult(i, 0);
 
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            for (String res: results) {
+                Log.i("Result", res);
+            }
+        }
+
     }
 }
