7fc7f32a9a1547345133d561b4adc49b2ed87ae9
Weronika Żak <weronikarzak@gmail.com>
2019-12-26 21:45:42 +0100

    wh

---
+++ WhatsAppClone/app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usernameText;
+    boolean loginMode;
+    EditText passwordText;
+
+
+    public void toggleLogin(View view) {
+        Button loginButton = findViewById(R.id.registerButton);
+        Button singinButton = findViewById(R.id.button);
+
+        if (loginMode) {
+            loginMode = false;
+            loginButton.setText("Or login");
+            singinButton.setText("Register");
+        } else {
+            loginMode = true;
+            loginButton.setText("Or register");
+            singinButton.setText("Login");
+        }
+    }
+
+    public void logIn(View view) {
+        if (loginMode) {
+            ParseUser.logInInBackground(usernameText.getText().toString(), passwordText.getText().toString(), new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (e == null) {
+
+                    } else {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            ParseUser user = new ParseUser();
+
+            user.setPassword(passwordText.getText().toString());
+            user.setUsername(usernameText.getText().toString());
+
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+
+                    } else {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameText = findViewById(R.id.usernameText);
+        passwordText = findViewById(R.id.passwordText);
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+}
